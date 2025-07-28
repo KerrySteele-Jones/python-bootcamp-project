418ec5e3689e3582597faab281e9d3208ea3789d
Kerry Steele-Jones <kerry@createxr.co.uk>
2025-07-28 18:36:09 +0100

    Started writing Fstrings - tested first two

--- main_start.py
+++ main_start.py
@@ -2,7 +2,16 @@ from generator.prompt_questions import ask_questions
 
 def main():
     answers =ask_questions()
-    print(answers)
 
+    read_me = f"""
+    ## Project Title 
+    {answers["project_title"]}
+
+    ## Description \n
+    {answers["description"]} 
+
+    """
+    print(read_me)
+    
 if __name__ == "__main__":
     main()
